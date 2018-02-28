0e4c8b61c50e2364546a139162c28e6ac98adce8
PRussell116 <33029749+PRussell116@users.noreply.github.com>
2018-02-28 17:44:09 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
 'use strict';
 console.log("connected");
-window.addEventListener('load',init);
+window.addEventListener('load', init);
 
-function init(){
+function init() {
   console.log("init");
-   const unitBlock = document.querySelectorAll('unitBlock');
+  const unitBlock = document.querySelectorAll('unitBlock');
 
-let weeks = document.querySelectorAll('#weeks .week');
-[].forEach.call(weeks, addDragDropHandlers);
-  // const weeks = document.querySelectorAll('week');
-//   for(let i = 0;i<weeks.length;i++){
-  //   addDnDHandlers(weeks[i]);
-//   }
-   // let weekSummaries = document.querySelectorAll('summary');
-   // for(let i = 0;i<weekSummaries.length;i++){
-   //   weekSummaries[i].addEventListener
+  let weeks = document.querySelectorAll('#weeks .week');
+  [].forEach.call(weeks, addDragDropHandlers);
 
-
-
-     //if(weekSummaries[i].contains == true){
-       //weekSummaries[i].content= "ABC";
-  //   }
-   //}
+  const deleteButts = document.getElementsByClassName('delete');
+  for(let i = 0;i<deleteButts.length;i++){
+    deleteButts[i].addEventListener('click',deleteWeek);
+  }
 
 
 }
+function deleteWeek(e){
+  selectedEle = this;
+  if(window.confirm("Are you sure you want to delete " + this.parentNode.firstChild.innerText)){
+    selectedEle.parentElement.remove(this);
+  }
+  // else dont do anything
+}
+
+
+
 let selectedEle = null;
 
-function handleDragStart(e){
+function handleDragStart(e) {
   selectedEle = this;
   e.dataTransfer.effectAllowed = 'move';
-  e.dataTransfer.setData('text/html',this.outerHTML);
+  e.dataTransfer.setData('text/html', this.outerHTML);
   this.classList.add('dragElem')
-  console.log("drag start");
-
 
 }
 function handleDragOver(e) {
@@ -44,40 +43,38 @@ function handleDragOver(e) {
 
   e.dataTransfer.dropEffect = 'move';
   return false;
-  console.log("drag over");
+
 }
-function handleDragLeave(e){
+function handleDragLeave(e) {
   this.classList.remove('over');
-  console.log("drag leave");
+
 }
 
-function handleDrop(e){
-  if(e.stopPropagation){
+function handleDrop(e) {
+  if (e.stopPropagation) {
     e.stopPropagation();
   }
-  if(selectedEle != this){
+  if (selectedEle != this) {
     this.parentNode.removeChild(selectedEle)
     let dropHTML = e.dataTransfer.getData('text/html');
-    this.insertAdjacentHTML('beforeBegin',dropHTML);
+    this.insertAdjacentHTML('beforeBegin', dropHTML);
     let dropElem = this.previousSibling;
     addDragDropHandlers(dropElem);
   }
   this.classList.remove('over');
   return false;
-  console.log("drop");
+
 }
 
-function handleDragEnd(e){
+function handleDragEnd(e) {
   this.classList.remove('over');
-  console.log("drag end");
+
 }
 
-function addDragDropHandlers(elem){
- elem.addEventListener('dragstart', handleDragStart, false);
- //elem.addEventListener('dragenter', handleDragEnter, false)
- elem.addEventListener('dragover', handleDragOver, false);
- elem.addEventListener('dragleave', handleDragLeave, false);
- elem.addEventListener('drop', handleDrop, false);
- elem.addEventListener('dragend', handleDragEnd, false);
- console.log("listeners added")
+function addDragDropHandlers(elem) {
+  elem.addEventListener('dragstart', handleDragStart, false);
+  elem.addEventListener('dragover', handleDragOver, false);
+  elem.addEventListener('dragleave', handleDragLeave, false);
+  elem.addEventListener('drop', handleDrop, false);
+  elem.addEventListener('dragend', handleDragEnd, false);
 }
