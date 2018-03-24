47d4bab9a781f0d5f8fa9a99e8e67951af94d59c
PRussell116 <33029749+PRussell116@users.noreply.github.com>
2018-03-24 19:02:17 +0000

    too many changes to list

--- server.js
+++ server.js
@@ -2,54 +2,124 @@ const express = require('express');
 const webSocket = require('ws');
 const http = require('http')
 const ip = require("ip");
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createConnection({host: 'localhost', user: 'root', password: 'root', charset: 'UTF8MB4', database: 'plannerDB'});
 
 const app = express();
 const port = 8080;
 
-
 const server = http.createServer(app);
 const wss = new webSocket.Server({server: server});
 
+wss.on('connection', connectionHandler);
 
-
-wss.on('connection',connectionHandler);
-
-function connectionHandler(ws){
+function connectionHandler(ws) {
   console.log("connected");
-  ws.on('message',messageHandler)
+  ws.on('message', messageHandler)
 
 }
 // combo with fetches
-function messageHandler(message){
+async function messageHandler(message, res) {
+  const sql = await connection;
+
   console.log('received: %s', message);
   let msgJson = JSON.parse(message);
-  if(msgJson.method == 'delete'){
-    // remove from DB
-    // change message to tell clients to delete
+  if (msgJson.method == 'delete') {
+    // remove unit from DB
+    if (msgJson.type == 'unit') {
+      // delete the unit (forigen key data items deleted aswell due to cascade delete)
+      await sql.query('DELETE FROM Unit WHERE unitId = ' + msgJson.element);
 
-  }
-  else if(msgJson.method == 'save'){
+    } else if (msgJson.type == 'week') {
+      // remove from DB by Id
+      await sql.query('DELETE FROM Week WHERE weekID = ' + msgJson.element);
+      const eleId = "week" + msgJson.element; // readd the week part of element to delete from page
+      console.log("eleid" + eleId);
+      console.log("unit" + msgJson.unit);
+      message = JSON.stringify({'method': 'delete', 'element': eleId, 'unit': msgJson.unit});
+
+    } else if (msgJson.type == 'resource') {
+      // remove from DB by Id
+      //      await sql.query('DELETE FROM weekResources WHERE   resourceId = ' + msgJson.element);
+      const eleId = "resource" + msgJson.element; // readd the resource part of element to delete from page
+      message = ({'method': 'delete', 'element': eleId});
+
+    }
+  } else if (msgJson.method == 'save') {
     // add to DB
+    const newWeekData = {
+      weekName: msgJson.title,
+      duration: msgJson.duration,
+      unitId: msgJson.unit
+    };
 
+    await sql.query(sql.format('INSERT INTO Week SET ?;', newWeekData));
+    let [weekId] = await sql.query('SELECT weekId FROM Week WHERE weekName LIKE  \"' + msgJson.title + "\" ");
+    let newId = "week" + weekId[0].weekId; // add week part of id
+    message = JSON.stringify({
+      'method': 'save',
+      'element': msgJson.element,
+      'title': msgJson.title,
+      'duration': msgJson.duration,
+      'unit': msgJson.unit,
+      'weekId': newId
+    });
   }
 
-
-  // forward message to clients
-  wss.clients.forEach((client)=>{
-    if(client.readyState === client.OPEN){
-      try{
+  // forward message to clients chnage as will add when on diff page
+  wss.clients.forEach((client) => {
+    if (client.readyState === client.OPEN) {
+      try {
         client.send(message);
-      }catch(err){
+      } catch (err) {
         // error ignored
       }
     }
   });
 }
 
+async function loadUnits(req, res) {
+  const sql = await connection;
+  try {
+    let [units] = await sql.query('SELECT * FROM Unit');
+    res.send(units);
+  } catch (e) {
+    error(res, e);
+  }
+}
 
-app.use(express.static(`${__dirname }/webpages`));
+async function getUnitContent(req, res) {
+  const sql = await connection;
+  console.log("unitId: " + req.query.unitId);
+  try {
+    // pull the weeks
+    let [weeks] = await sql.query(sql.format('SELECT * FROM Week WHERE unitId LIKE ' + req.query.unitId));
 
+    //pull the resources for the weeks
+    let resources = [];
+    for (let i = 0; i < weeks.length; i++) {
+      let [weekResources] = await sql.query('SELECT * FROM WeekResources WHERE weekId = ' + weeks[i].weekId);
+      resources += await weekResources;
+    }
+    res.json({weeks: weeks, resources: resources});
+
+  } catch (e) {
+    error(res, e);
+  }
+
+}
+
+function error(res, msg) {
+  res.sendStatus(500);
+  console.error(msg);
+}
+
+app.use(express.static(`${__dirname}/webpages`));
+//app.get('/week',loadWeeks);
+app.get('/unit', loadUnits);
+app.get('/unitContent', getUnitContent);
 
 server.listen(port, () => {
-    console.log('Server started:', `http://${ip.address()}:${port}` )
+  console.log('Server started:', `http://${ip.address()}:${port}`)
 });
--- webpages/index.js
+++ webpages/index.js
@@ -1,74 +1,140 @@
 'use strict';
 console.log("connected");
 window.addEventListener('load', init);
-let ws = new WebSocket("ws://" + window.location.hostname + ":" + (window.location.port || 80) + "/");
-ws.addEventListener("message",recivedMessageFromServer);
+let ws = new WebSocket("ws://" + window.location.hostname + ":" + (
+window.location.port || 80) + "/");
+ws.addEventListener("message", recivedMessageFromServer);
 
+async function init() {
+  await loadUnits();
 
-function init() {
   console.log("init");
   const unitBlock = document.querySelectorAll('unitBlock');
 
+  // add drag + drop handlers for blocks
   let weeks = document.querySelectorAll('#weeks .week');
   [].forEach.call(weeks, addDragDropHandlers);
-
+  // listners to delete buttons
   const deleteButts = document.getElementsByClassName('delete');
-  for(let i = 0;i<deleteButts.length;i++){
-    deleteButts[i].addEventListener('click',deleteWeek);
+  for (let i = 0; i < deleteButts.length; i++) {
+    deleteButts[i].addEventListener('click', deleteWeek);
   }
-
+  // listeners to add buttons
   const addButts = document.getElementsByClassName('add');
-  for(let i = 0;i<addButts.length;i++){
-    addButts[i].addEventListener('click',addWeekInputs);
+  for (let i = 0; i < addButts.length; i++) {
+    addButts[i].addEventListener('click', addWeekInputs);
   }
+}
+// get json of the units from DB then call funct to put them in page
+async function loadUnits() {
+  let unitJson = await fetch('/unit');
+  putUnitsInPage(await unitJson.json());
+}
+// add the units to the page using template and json from DB
+function putUnitsInPage(units) {
+  const template = document.querySelector('#unit_t');
+  for (let i = 0; i < units.length; i++) {
+    let newEl = document.importNode(template.content, true).children[0];
+    let newElA = newEl.querySelector('.unitLink');
+    newElA.innerText = units[i].unitName;
+    newElA.addEventListener('click', unitClickHandler);
 
+    // put the unit id in hidden p
+    newEl.querySelector('.hidden').innerText = units[i].unitId;
+    newEl.querySelector('.delete').addEventListener('click', deleteUnit);
+
+    document.getElementById('unitsBar').appendChild(newEl);
+  }
+}
+
+function deleteUnit(e) {
+  let eleToDelete = this.parentNode
+  eleToDelete.remove();
+  let delId = eleToDelete.querySelector('p').innerText; // get id held in p
+
+  const unit = document.getElementById('currentUnitId').textContent;
+
+  // send Delete event to server
+  ws.send(JSON.stringify({'method': 'delete', 'element': delId, 'type': 'unit', 'unit': unit}));
+}
+
+function deleteElement(eleID) {
+  document.getElementById(eleID).remove();
+}
+
+// send request for a unit's contents then call function to put in page
+async function unitClickHandler(e) {
+  const unitId = this.parentNode.querySelector('.hidden').textContent;
+  document.getElementById('currentUnitId').textContent = unitId;
+  document.getElementById('currentUnit').innerText = this.textContent;
+
+  let unitContentJson = await fetch('/unitContent/?unitId=' + unitId);
+  putUnitContentInPage(await unitContentJson.json());
+}
+
+function putUnitContentInPage(content) {
+  // delete old weeks
+  let delWeeks = document.querySelectorAll('.week')
+  delWeeks.forEach((week) => {
+    week.remove();
+  });
+  // add new weeks
+  let newWeeks = content.weeks
+  let tempWeek = document.createElement('li');
+  tempWeek.setAttribute('id', 'tempWeek');
+  document.getElementById("weeks").appendChild(tempWeek);
+
+  for (let i = 0; i < newWeeks.length; i++) {
+    let newId = "week" + newWeeks[i].weekId
+    addWeekToPage("tempWeek", newWeeks[i].weekName, newWeeks[i].duration, newId);
+
+  }
+  tempWeek.remove();
 
 }
-function deleteWeek(e){
+
+function deleteWeek(e) {
   console.log("delete event start")
   selectedEle = this;
 
   let confirmBoxContain = document.createElement("section");
-  confirmBoxContain.setAttribute("class","decisonContainer");
+  confirmBoxContain.setAttribute("class", "decisonContainer");
 
   let yesBox = document.createElement("p");
   yesBox.innerText = "yes"
-  yesBox.setAttribute("class","decisonBox");
+  yesBox.setAttribute("class", "decisonBox");
 
   let noBox = document.createElement("p");
   noBox.innerText = "no"
-  noBox.setAttribute("class","decisonBox");
+  noBox.setAttribute("class", "decisonBox");
 
   confirmBoxContain.appendChild(yesBox);
   confirmBoxContain.appendChild(noBox);
 
-   yesBox.addEventListener('click',yesNoHandler);
-   noBox.addEventListener('click',yesNoHandler);
+  yesBox.addEventListener('click', yesNoHandler);
+  noBox.addEventListener('click', yesNoHandler);
 
   this.parentNode.appendChild(confirmBoxContain);
 
-
-  selectedEle.removeEventListener('click',deleteWeek);
+  selectedEle.removeEventListener('click', deleteWeek);
 }
 
-
-function addWeekInputs(e){
+function addWeekInputs(e) {
   console.log("add week event started");
 
   let addBox = document.createElement('article');
 
   let weekToAddTo = document.createElement('p');
   weekToAddTo.innerText = this.parentNode.id; // gets the week
-  weekToAddTo.setAttribute('id',"weekAdd");
+  weekToAddTo.setAttribute('id', "weekAdd");
 
   let weekTitleBox = document.createElement('input');
   weekTitleBox.type = "text";
   weekTitleBox.innerText = "week title";
-  weekTitleBox.setAttribute('id',"titleBox");
+  weekTitleBox.setAttribute('id', "titleBox");
 
   let weekTitleText = document.createElement('p');
   weekTitleText.innerText = "Title:"
-
 
   let durationText = document.createElement('p')
   durationText.innerText = "Duration:"
@@ -76,17 +142,17 @@ function addWeekInputs(e){
   let durationBox = document.createElement('input');
   durationBox.type = "number";
   durationBox.innerText = "duration";
-  durationBox.setAttribute('id',"duration");
+  durationBox.setAttribute('id', "duration");
 
   let saveBox = document.createElement('p')
   saveBox.classList.add('save');
   saveBox.innerText = "Save";
-  saveBox.addEventListener('click',saveHandler);
+  saveBox.addEventListener('click', saveHandler);
 
   let cancelBox = document.createElement('p');
   cancelBox.classList.add('cancel');
   cancelBox.innerText = "cancel";
-  cancelBox.addEventListener('click',cancelHandler);
+  cancelBox.addEventListener('click', cancelHandler);
 
   addBox.appendChild(weekToAddTo);
   addBox.appendChild(weekTitleText);
@@ -95,92 +161,100 @@ function addWeekInputs(e){
   addBox.appendChild(durationBox);
   addBox.appendChild(saveBox);
   addBox.appendChild(cancelBox);
-  addBox.setAttribute('id',"addWeekBox");
+  addBox.setAttribute('id', "addWeekBox");
 
   document.querySelector('.wrapper').appendChild(addBox);
-  this.removeEventListener('click',addWeekInputs);
+  this.removeEventListener('click', addWeekInputs);
 
 }
 
-function saveHandler(e){
-  // get content from boxes
-  let weekToAddTo = document.getElementById('weekAdd');
-  let title = document.getElementById('titleBox').value;
-  let duration = document.getElementById('duration').value;
-  ws.send(
-    JSON.stringify ({
-      'method'  : 'save',
-      'element' : weekToAddTo.innerText,
-      'title'   : title,
-      'duration': duration
-    }));
-
-     document.getElementById('addWeekBox').remove();
-     // re-add the event listener to the + button
-     document.getElementById(weekToAddTo.innerText).querySelector('.add').addEventListener('click',addWeekInputs);
-
- }
-
-// after clicking cancel the box is removed
-function cancelHandler(e){
-   document.getElementById('addWeekBox') .remove();
-}
-
-
- function yesNoHandler(e){
-    selectedEle = this;
-    let elementToDelete = selectedEle.parentNode.parentNode;
-    if (selectedEle.innerText == "yes"){
-    // tell server to delete
-    ws.send(
-      JSON.stringify ({
-        'method' : 'delete',
-        'element' : elementToDelete.getAttribute('id')
-      }));
-   }
-   else{
-     // delete the yes / no box
-     elementToDelete.querySelector(".delete").addEventListener('click',deleteWeek);// readd event listner to delete button
-     elementToDelete.removeChild(selectedEle.parentNode); // delete the yes / no box container when done
-
-   }
-}
-
-
-function deleteElement(eleID){
-  document.getElementById(eleID).remove();
-}
-
-function addWeekToPage(prevEle,title,duration){
+function addWeekToPage(prevEle, title, duration, weekId) {
 
   const template = document.querySelector('#week_t');
-  const newEl = document.importNode(template.content,true).children[0];
+  const newEl = document.importNode(template.content, true).children[0];
 
-  newEl.querySelectorAll('.delete').forEach((button)=>{
-    button.addEventListener('click',deleteWeek);
+  newEl.querySelectorAll('.delete').forEach((button) => {
+    button.addEventListener('click', deleteWeek);
   });
+
+  newEl.querySelectorAll('.add').forEach((button) => {
+    button.addEventListener('click', addWeekInputs);
+  });
+
   newEl.querySelector('details > summary').innerText = title;
 
   addDragDropHandlers(newEl);
+  const newId = "week" + weekId;
+  newEl.setAttribute('id', weekId);
 
-  document.getElementById(prevEle).insertAdjacentElement('afterEnd',newEl);
+  document.getElementById(prevEle).insertAdjacentElement('afterEnd', newEl);
   // do something with duration
 
 }
 
-function recivedMessageFromServer(e){
-  const recived = JSON.parse(e.data);
-  if (recived.method == "delete"){
-    deleteElement(recived.element);
-  }
-  else if(recived.method == "save"){
-    addWeekToPage(recived.element,recived.title,recived.duration);
-  }
+function saveHandler(e) {
+  // get content from boxes
+  let weekToAddTo = document.getElementById('weekAdd');
+  let title = document.getElementById('titleBox').value;
+  let duration = document.getElementById('duration').value;
+  let unit = document.getElementById('currentUnitId').textContent;
+  ws.send(JSON.stringify({'method': 'save', 'element': weekToAddTo.innerText, 'title': title, 'duration': duration, 'unit': unit}));
 
+  document.getElementById('addWeekBox').remove();
+  // re-add the event listener to the + button
+  document.getElementById(weekToAddTo.innerText).querySelector('.add').addEventListener('click', addWeekInputs);
 
 }
 
+// after clicking cancel the box is removed
+function cancelHandler(e) {
+  document.getElementById('addWeekBox').remove();
+}
 
+function yesNoHandler(e) {
+  selectedEle = this;
+  let elementToDelete = selectedEle.parentNode.parentNode;
+  let type = "";
+  let delId = "";
+  if (elementToDelete.classList.contains("week")) {
+    type = "week";
+    delId = elementToDelete.getAttribute('id').slice(4); // remove week part from id e.g week24 -> 24
+    console.log("del Id: " + delId);
+  } else if (elementToDelete.classList.contains("resource")) {
+    type = "resource";
+    delId = elementToDelete.getAttribute('id').slice(8); // remove resource part from id e.g resource24 -> 24
+    console.log("del Id: " + delId);
+  }
+  const unit = document.getElementById('currentUnitId').textContent;
+  if (selectedEle.innerText == "yes") {
+    // tell server to delete
+
+    ws.send(JSON.stringify({'method': 'delete', 'element': delId, 'type': type, 'unit': unit}));
+  } else {
+    // delete the yes / no box
+    elementToDelete.querySelector(".delete").addEventListener('click', deleteWeek); // readd event listner to delete button
+    elementToDelete.removeChild(selectedEle.parentNode); // delete the yes / no box container when done
+
+  }
+}
+
+function recivedMessageFromServer(e) {
+  console.log("message recived");
+  const recived = JSON.parse(e.data);
+  // prevent adding to wrong pages
+  console.log("unit" + document.getElementById('currentUnitId').textContent);
+  if (recived.unit == document.getElementById('currentUnitId').textContent) {
+
+    if (recived.method == "delete" && recived.type != "unit") {
+      deleteElement(recived.element);
+    } else if (recived.method == "save") {
+      addWeekToPage(recived.element, recived.title, recived.duration, recived.weekId);
+      console.log("weekId line 283" + recived.weekId);
+    }
+
+  }
+
+}
 
 let selectedEle = null;
 
@@ -225,8 +299,8 @@ function handleDrop(e) {
     let dropElem = this.previousSibling;
     let innerDeletes = dropElem.getElementsByClassName('delete');
     // readd event listners
-    for(let i = 0;i<innerDeletes.length;i++){
-      innerDeletes[i].addEventListener('click',deleteWeek);
+    for (let i = 0; i < innerDeletes.length; i++) {
+      innerDeletes[i].addEventListener('click', deleteWeek);
     }
     addDragDropHandlers(dropElem);
   }
@@ -239,9 +313,6 @@ function handleDragEnd(e) {
   this.classList.remove('over');
 
 }
-
-
-
 
 function addDragDropHandlers(elem) {
   elem.addEventListener('dragstart', handleDragStart, false);
